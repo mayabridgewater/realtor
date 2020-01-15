844ba540caef4e939d204767b8257016c2774eef
mayabridgewater <55682563+mayabridgewater@users.noreply.github.com>
2020-01-15 13:03:59 +0200

    need to pull master

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import './components/style/style.css';
 import Gallery from "./components/gallery/gallery";
 import Header from "./components/header/header";
 import Search from "./components/filters/search";
-// import {apartments} from "./components/gallery/apartments";
-// import {cities} from "./components/gallery/cities";
-import {cityFilter} from "./components/filters/functions/cityFilter";
-import {priceFilter} from "./components/filters/functions/priceFilter";
-import {bedsFilter} from "./components/filters/functions/bedsFilter";
-import {bathsFilter} from "./components/filters/functions/bathsFilter";
-// import {propertyFilter} from "./components/filters/functions/propertyFilter";
 import {
     BrowserRouter as Router,
     Switch,
@@ -42,6 +35,7 @@ class App extends React.Component {
     };
 
     handleSuccess = (success) => {
+        console.log(success);
         this.setState({
             apartments: success,
             updatedApartments: success,
