d51b243b6c15f8dd2a6c67dab581cb10314f5326
maya <bridgewatermaya@gmail.com>
2020-01-18 16:25:05 +0200

    moved to something else

--- src/components/header/rightNav.jsx
+++ src/components/header/rightNav.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Cookies from 'js-cookie';
+
 import PhoneNav from "./phoneNav";
 import Login from './login';
 import Signup from "./signup";
@@ -8,7 +10,17 @@ class RightNav extends React.Component {
         super();
         this.state = {
             display: false,
-            showForm: 2//null
+            showForm: null,
+            roleId: null //3-admin 4-user
+        }
+    }
+
+    componentDidMount() {
+        const currentCookie = JSON.parse(Cookies.get('user'));
+        if (currentCookie) {
+            this.setState({
+                roleId: currentCookie.role_id
+            })
         }
     }
 
@@ -31,17 +43,29 @@ class RightNav extends React.Component {
       }
     };
 
+    login = (id) => {
+        this.setState({
+            roleId: id
+        })
+    }
+
     render() {
+        console.log(this.state)
         return (
             <div className={'rightNav d-flex'}>
                 <div className={'img d-none d-md-flex'} onMouseEnter={this.showStyle} onMouseLeave={() => this.showStyle(false)}>
                     <img src={'./images/phoneicon.png'} width={'30px'} alt={''}/>
                     {this.state.display && <PhoneNav/>}
                 </div>
-                <p onClick={() => this.handleForm(1)} style={{padding: '0 5px'}}>Log In</p>
-                {this.state.showForm === 1 && <Login handleForm={this.handleForm}/>}
-                <p onClick={() => this.handleForm(2)} style={{padding: '0 5px'}}>Sign Up</p>
-                {this.state.showForm === 2 && <Signup handleForm={this.handleForm} check={this.unCheck}/>}
+                {!this.state.roleId &&
+                <div className='d-flex'>
+                    <p onClick={() => this.handleForm(1)} style={{padding: '0 5px'}}>Log In</p>
+                    {this.state.showForm === 1 && <Login handleForm={this.handleForm} login={this.login}/>}
+                    <p onClick={() => this.handleForm(2)} style={{padding: '0 5px'}}>Sign Up</p>
+                    {this.state.showForm === 2 && <Signup handleForm={this.handleForm} check={this.unCheck}/>}
+                </div>
+                }
+                {this.state.roleId && <p>Logout</p>}
                 <span className={'d-none d-md-flex'}>|</span>
                 <p className={'d-none d-md-flex'}><a href={"/"}>Advertise</a></p>
             </div>
