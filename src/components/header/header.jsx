a63a74b96eb53c256e86b249ebf110e942ab3b56
mayabridgewater <55682563+mayabridgewater@users.noreply.github.com>
2020-01-26 13:17:15 +0200

    header deleted

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import {navigation} from "./navigation";
-import DropDownNav from "./dropDownNav";
 import RightNav from "./rightNav";
 import {Link} from "react-router-dom";
 
@@ -30,9 +28,6 @@ class Header extends React.Component {
                     {this.state.burgerMenu && <div className={'rightBurger'} onClick={this.showSideMenu}/>}
                     <Link to={'/'}><img alt={'Realtor.com'} src={'./images/logo.png'} style={{width: '180px'}}/></Link>
 
-                    <div className={'menu'}>
-                        {navigation.map((item, i) => <DropDownNav {...item} burgerMenu={this.state.burgerMenu} key={i}/>)}
-                    </div>
 
                 </div>
                 <div>
